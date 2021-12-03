c86d75532ff052b302cfd75ba0a54adc00e2a2b6
Isaac Valdespino <isaac.est.cbtis@gmail.com>
2021-12-02 22:37:10 -0600

    exercise 2

---
+++ 02_WebDevelopment/E2/GymManager.Web/GymManager.Web/Models/City.cs
@@ -0,0 +1,11 @@
+﻿namespace GymManager.Web.Models
+{
+    public class City
+    {
+
+        public int Id { set; get; }
+
+        public string Name { set; get; }
+
+    }
+}
---
+++ 02_WebDevelopment/E2/GymManager.Web/GymManager.Web/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿namespace GymManager.Web.Models
+{
+    public class Member
+    {
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public int CityId { get; set; }
+
+        public string Email { get; set; }
+
+        public bool AllowNewsLetter { get; set; }
+
+    }
+}
