0bf74952167d9bc8bb9a2abcd668c7a763c5194d
Isaac Valdespino <isaac.est.cbtis@gmail.com>
2021-12-04 20:17:22 -0600

    exercice 4

---
+++ 02_WebDevelopment/E4/GymManager.Web/GymManager.Web/Controllers/AttendanceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.Web.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MemberIn()
+        {
+            return View("MemberIn");
+        }
+        public IActionResult MemberOut()
+        {
+            return View(MemberOut);
+        }
+    }
+}
