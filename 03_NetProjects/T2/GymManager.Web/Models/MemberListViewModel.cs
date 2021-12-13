87667b66773e7fd0e2886c683cd8a5c697e9500e
Isaac Valdespino <isaac.est.cbtis@gmail.com>
2021-12-12 20:48:49 -0600

    segundo tutorial

---
+++ 03_NetProjects/T2/GymManager.Web/Models/MemberListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GymManager.Core.Members;
+
+namespace GymManager.Web.Models
+{
+    public class MemberListViewModel
+    {
+        public int NewMembersCount { get; set; }
+
+        public List<Member> Members { get; set; }
+    }
+}
