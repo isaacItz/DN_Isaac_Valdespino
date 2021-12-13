3af98d24ee4e2f7e34246e27839b508fb59d1e73
Isaac Valdespino <isaac.est.cbtis@gmail.com>
2021-12-12 18:38:39 -0600

    primer tutorial

---
+++ 03_NetProjects/T1/GymManager.Web/Controllers/AttendanceController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.Web.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("MemberIn");
+        }
+        public IActionResult MemberIn()
+        {
+            return View("MemberIn");
+        }
+        public IActionResult MemberOut()
+        {
+            return View(MemberOut);
+        }
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ 03_NetProjects/T1/GymManager.Web/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+app.UseStaticFiles();
+
+app.Run();
