c53cd6be64bec04995e8e8219ecd0017670ba19f
Isaac Lugo <isaac.est.cbtis@gmail.com>
2022-01-10 23:03:05 -0600

    changes

--- 05_Stored_Procedures/E3/test_sale_products.sql
+++ 05_Stored_Procedures/E3/test_sale_products.sql
@@ -1,3 +1,5 @@
+/* pasamos dos listas, una con los id de productos que se van a vender
+y otra lista con la cantidad que venderemos por cada producto */
 set @list = "1,2,3";
 set @list2 = "1,2,4";
 
